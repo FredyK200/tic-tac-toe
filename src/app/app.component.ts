03dc53867bf7abdb69579db7986d9e0de1889931
FredyK200 <qwertygeek01@gmail.com>
2020-04-25 20:56:56 -0400

    Added Project

---
+++ src/app/app.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit } from '@angular/core';
+import io from "socket.io-client";
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+
+  private socket: any;
+  public name;
+  public players = {playerX: false, playerO: false};
+  public player;
+  public turn;
+  public connected=true;
+  public gameName;
+
+
+  public board = [
+    ["","",""],
+    ["","",""],
+    ["","",""]
+  ];
+
+  public ngOnInit() {
+    this.getServerData();
+  }
+
+  public getServerData() {
+    this.socket = io("https://kaneiscool.herokuapp.com/");
+    this.socket.on("board", board => {
+      this.board = board;
+    });
+    this.socket.on("turn", turn => {
+      this.turn = turn;
+    })
+    this.socket.on("clearBoard", player => {
+      this.player = "";
+      alert(player + " Cleared The Board");
+    });
+    this.socket.on("players", players => {
+      this.players = players;
+    });
+    this.socket.on("winner", winner => {
+      if (winner != ""){
+        alert(winner + " Wins!"); 
+      }
+    });
+  }
+
+  public setName(event: Event) {
+    this.name = (event.target as HTMLInputElement).value;
+  }
+
+  public move(selection) {
+    if (this.player == this.turn){
+      this.socket.emit("move",[selection, this.player, this.name]);
+    }
+  }
+
+  public clearBoard(){
+    this.socket.emit("clearBoard", this.name);
+  }
+
+  public selectPlayer(selection){
+    this.player = selection;
+    this.socket.emit("selection", selection);
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field'
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatInputModule,
+    MatFormFieldModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
